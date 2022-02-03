cc3ebd1524f19038900f97381c466d18e2139b51
AlphaYoung111 <729346015@qq.com>
2022-02-03 20:08:01 +0800

    test:lint

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <div     class="out">
+    <div    class="inner">123</div>
+  </div>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+
+
+
+
+
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.out {
+  .inner {
+    color: red;
+  }
+}
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    AutoImport({
+      imports: ['vue', 'vue-router'],
+      // 可以选择auto-import.d.ts生成的位置，使用ts建议设置为'src/auto-import.d.ts'
+      dts: 'src/auto-import.d.ts'
+    })
+  ]
+})
